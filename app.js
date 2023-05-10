b8b9707d938076ce0336af9498a902a629808908
BeomSu-Kang <kbs21c1987@gmail.com>
2023-05-10 23:09:20 +0900

    몽고디비 연결

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 const express = require("express");
 
+const db = require("./data/database");
 const authRoutes = require("./routes/auth.routes");
 
 const app = express();
@@ -13,4 +14,12 @@ app.use(express.static("public"));
 
 app.use(authRoutes);
 
-app.listen(3000);
+db.connectToDatabase()
+  .then(() => {
+    //데이터베이스 연결되면 호출
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error);
+    console.log("데이터베이스 연결실패!!");
+  });
